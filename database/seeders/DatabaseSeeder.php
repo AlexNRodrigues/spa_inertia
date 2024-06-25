facc87e3acea68f9a6ee1dc4630ecbaaddec379e
AlexNRodrigues <alexnasscimento99@gmail.com>
2024-06-24 21:19:55 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $categories = Category::factory(10)->create();
+        
+        User::factory(5)
+            ->has(
+                Product::factory(50)->state(function() use ($categories) {
+                    return ['category_id' => $categories->random()->id];
+                })
+            )
+            ->create();
+        
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
